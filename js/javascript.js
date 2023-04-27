a33f019eb8447de5b080d4b4480332c8c8afdae0
irongael <126038141+Irongael@user.noreply.github.com>
2023-04-26 22:50:35 -0400

    add function to clear old grid

--- js/javascript.js
+++ js/javascript.js
@@ -2,17 +2,24 @@ function createGrid() {
     const container = document.querySelector('#container');
     let num1 = prompt("please enter grid size", 1);
 
-    container.style.gridTemplateColumns = `repeat(${num1}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${num1}, 1fr)`;
+    if((num1 > 0) && (num1 < 101)) {
+        container.style.gridTemplateColumns = `repeat(${num1}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${num1}, 1fr)`;
     
-    let j = getGridSize(num1);
+        let j = getGridSize(num1);
 
-    for(let i = 0; i < j; i++) {
-        const content = document.createElement('div');
-        content.classList.add('content');
+        for(let i = 0; i < j; i++) {
+            const content = document.createElement('div');
+            content.classList.add('content');
 
-        container.appendChild(content);
+            container.appendChild(content);
+        }
     }
+    else {
+        console.log("ERROR");
+    }
+
+    
 }
 
 function getGridSize(num) {
@@ -20,4 +27,18 @@ function getGridSize(num) {
     return num;
 }
 
-createGrid();
+function clearGrid() {
+    const container = document.querySelector('#container');
+
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+}
+
+const gridBtn = document.querySelector('#btn');
+gridBtn.addEventListener('click', () => {
+    clearGrid();
+    createGrid();
+});
+
