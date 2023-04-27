8b92dda9e8128c2d668f9b577115218956f1ef26
irongael <126038141+Irongael@user.noreply.github.com>
2023-04-26 22:00:17 -0400

    basic grid generation working

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,23 @@
 function createGrid() {
     const container = document.querySelector('#container');
+    let num1 = prompt("please enter grid size", 1);
 
-    for(let i = 0; i < 256; i++) {
+    container.style.gridTemplateColumns = `repeat(${num1}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num1}, 1fr)`;
+    
+    let j = getGridSize(num1);
+
+    for(let i = 0; i < j; i++) {
         const content = document.createElement('div');
         content.classList.add('content');
 
         container.appendChild(content);
     }
+}
+
+function getGridSize(num) {
+    num = num * num;
+    return num;
 }
 
 createGrid();
